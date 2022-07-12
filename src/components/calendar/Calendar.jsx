ebb37f5092f43193a0f83858fef001e06044e8b6
ArDezZz <aromudez@gmail.com>
2022-07-12 13:25:39 +0300

    some fixes with create event

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -41,7 +41,7 @@ const Calendar = ({ weekDates, isVisible, showModalMenu }) => {
         }
         throw new Error();
       })
-      .then(fetchEventsList())
+      .then(res => fetchEventsList())
       .catch(() => {
         alert("Internal Server Error. Can't display events");
       });
